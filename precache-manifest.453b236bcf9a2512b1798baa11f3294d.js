9a59d2930239da336a4d59e535bd69a84cbb237d
sverrkju <kjuus@live.no>
2021-01-04 19:58:16 +0100

    Updates

---
+++ precache-manifest.453b236bcf9a2512b1798baa11f3294d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4002ca8100e1c1b32fdf67894d7b7c5",
+    "url": "/Prog_i_Geo/index.html"
+  },
+  {
+    "revision": "e97c80b2252222870ee9",
+    "url": "/Prog_i_Geo/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "17a2e783db11d6f990ba",
+    "url": "/Prog_i_Geo/static/css/main.af7a9bcc.chunk.css"
+  },
+  {
+    "revision": "e97c80b2252222870ee9",
+    "url": "/Prog_i_Geo/static/js/2.6738fa00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Prog_i_Geo/static/js/2.6738fa00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a2e783db11d6f990ba",
+    "url": "/Prog_i_Geo/static/js/main.a3415de9.chunk.js"
+  },
+  {
+    "revision": "e6da3ca61e2a21ce000d",
+    "url": "/Prog_i_Geo/static/js/runtime-main.dad1fdb5.js"
+  }
+]);
